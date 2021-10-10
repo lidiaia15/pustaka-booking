c738997362cb82ddc4718d3fdb02bacbdfa35a60
lidiaia15 <maulidia261531@gmail.com>
2021-10-10 18:22:02 +0700

    latihan2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama maulidia hasanah
+ Saya tingga di daerah teluknaga
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
