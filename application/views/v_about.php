d30b725e17f228ca68f1deb5ed673826e29b1d0a
lidiaia15 <maulidia261531@gmail.com>
2021-10-24 16:34:22 +0700

    Latihan4

---
+++ application/views/v_about.php
@@ -0,0 +1,19 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: maulidia</li>
+        <li>Nama Belakang: hasanah</>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> pintu kapuk rt 23 rw 11 no. 25</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>joging</li>
+    </ul>
+</section>
